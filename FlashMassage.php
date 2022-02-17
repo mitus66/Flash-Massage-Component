e9922e369c5b09ea8129bb2cfe43f7e523d7187c
Dmitriy Maslakov <dmitry.maslakov@gmail.com>
2022-02-17 15:28:54 +0200

    initial

---
+++ FlashMassage.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class FlashMassage
+{
+    public $flashName;
+    public $content;
+
+    function set($flashName, $content)
+    {
+        $this->flashName = $flashName;
+        $this->content = $content;
+
+        $_SESSION[$flashName] = $content;
+
+        return $_SESSION[$flashName];
+    }
+
+    function display($flashName)
+    {
+        if(isset($_SESSION[$flashName])) {
+            echo $_SESSION[$flashName];
+            unset($_SESSION[$flashName]);
+        }
+
+    }
+
+}
+
+//$flashMassage = new FlashMassage();
+
+//$flashMassage->set('danger', 'Логин или пароль введены неверно');
+//$flashMassage->display('success');
